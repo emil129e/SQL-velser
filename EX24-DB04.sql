43812d4bd2ea0027ba4fc370a188eb3a2a795358
emil129e <43535233+emil129e@users.noreply.github.com>
2018-11-14 12:56:52 +0100

    Add files via upload

--- EX24-DB04.sql
+++ EX24-DB04.sql
@@ -1,27 +1,44 @@
 USE C_DB05_2018;
 INSERT INTO PET_OWNER 
-(OwnerID, OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)
+(OwnerLastName, OwnerFirstName, OwnerPhone, OwnerEmail)
 VALUES 
-(1, 'Downs', 'Marscha', 5555378765, 'Marsha.downs@somewhere.com'), 
-(2,	'James', 'Richard', 5555377654,	'Richard.James@somewhere.som'),
-(3,	'Frier', 'Liz',	5555376543,	'Liz.Frier@somewhere.com'), 
-(4,	'Trent', 'Miles', NULL,	'Miles.Trent@somewhere.com');
-
-SELECT * FROM PET_OWNER 
-ALTER TABLE PET_OWNER ALTER COLUMN OwnerPhone INT;
-/*
-ALTER TABLE PET ALTER COLUMN PetDOB DATETIME2;
-ALTER TABLE PET ALTER COLUMN PetWeight FLOAT;
-*/
-
+('Downs', 'Marscha', '5555378765', 'Marsha.downs@somewhere.com'), 
+('James', 'Richard', '5555377654', 'Richard.James@somewhere.som'),
+('Frier', 'Liz', '5555376543', 'Liz.Frier@somewhere.com'), 
+('Trent', 'Miles', NULL, 'Miles.Trent@somewhere.com');
 
 INSERT INTO PET
-(PetID,	PetName,	PetType,	PetBreed,	PetDOB,	PetWeight,	OwnerId)
+(PetName, PetType, PetBreed, PetDOB, PetWeight, OwnerId)
 VALUES
-(1,	'King',	'Dog',		'Std. Poodle',	20110221,	25.5,	1);
-(2, 'Teddy', 'Cat', 'Cashmere',	01-02-2012,	10.5,	2),
-(3,	'Fido',	'Dog', 'Std. Poodle',	17-07-2010, 28.5,	1), 
-(4,	'AJ',	'Dog',	'Collie Mix',	05/05-2011,	20.0,	3),
-(5,	'Cedro',	'Cat',	null,	06/06/2009,	9.5,	2), 
-(6,	'Wooley',	'Cat',	null, null, 9.5, 2),
-(7,	'Buster',	'Dog',	'Border Collie',	11/12/2008,	25.0,	4);
+('King', 'Dog', 'Std. Poodle', '21.02.2011', 25.5, 1),
+('Teddy', 'Cat', 'Cashmere', '01.02.2012', 10.5, 2),
+('Fido', 'Dog', 'Std. Poodle', '17.07.2010', 28.5,	1), 
+('AJ', 'Dog',	'Collie Mix', '05.06.2011', 20.0,	3),
+('Cedro', 'Cat', default, '06.06.2009', 9.5, 2), 
+('Wooley', 'Cat', default, null, 9.5, 2),
+('Buster', 'Dog', 'Border Collie', '11.12.2008', 25.0, 4);
+
+/*ØVELSE 2 DB04*/
+SELECT PetId, PetName, PetType, PetBreed, PetDOB, PetWeight, OwnerID FROM PET /* 3.15*/
+SELECT * FROM PET /* 3.16*/
+SELECT PetBreed, PetType FROM PET /* 3.17*/
+SELECT PetBreed, PetType, PetDOB FROM PET WHERE PetType ='Dog', 'Cat' /* 3.18*/
+SELECT PetBreed FROM PET /* 3.19*/
+SELECT DISTINCT PetBreed FROM PET /* 3.20*/
+SELECT PetBreed, PetType, PetDOB FROM PET WHERE PetType ='Dog' AND PetBreed='Std. Poodle' /* 3.21*/
+SELECT PetName,PetBreed , PetType FROM PET WHERE PetType !='Dog' AND PetType <> 'Cat' AND PetType !='Fish' /* 3.22*/
+/*ØVELSE 4 DB05*/
+SELECT OwnerFirstName, OwnerLastName, OwnerEmail FROM PET_OWNER WHERE OwnerEmail LIKE '%somewhere.com'; /* 3.24*/
+SELECT PetName, PetBreed FROM PET ORDER BY PetName; /* 3.26*/
+SELECT COUNT(DISTINCT PetBreed) FROM PET; /* 3.29*/
+SELECT MIN(PetWeight) AS "MinWeight", MAX(PetWeight) AS "MaxWeight", AVG(PetWeight) AS "AVGWeight" FROM PET; /* 3.31*/
+SELECT AVG(PetWeight) AS "AVGWeight", PetBreed FROM PET GROUP BY PetBreed; /* 3.32*/
+SELECT AVG(PetWeight) AS "AVGWeight", PetBreed FROM PET GROUP BY PetBreed HAVING COUNT(*) >1; /* 3.33*/
+SELECT AVG(PetWeight) AS "AVGWeight", PetBreed FROM PET WHERE PetBreed <> 'Unknown' GROUP BY PetBreed HAVING COUNT(*) >1; /* 3.34*/
+/*ØVELSE 6 DB05*/
+SELECT OwnerLastName, OwnerFirstName, OwnerEmail FROM PET_OWNER WHERE OwnerID IN(SELECT OwnerID FROM PET WHERE PetType='Cat'); /* 3.35*/
+
+
+
+
+
--- EX2403.sql
+++ EX2403.sql
@@ -1,43 +1,43 @@
-/*DROP TABLE PET_OWNER;*/
+/*DROP TABLE PET;
+DROP TABLE PET_OWNER;
+DROP TABLE LINE_ITEM;
+DROP TABLE INVOICE;
+DROP TABLE PRODUCT; 
+DROP TABLE SEMINAR_CUSTOMER;
+DROP TABLE SEMINAR;*/
 
 CREATE TABLE PET_OWNER(
 OwnerID int not null IDENTITY (1,1),
-OwnerLastNamen nvarchar(max) NOT NULL,
+OwnerLastName nvarchar(max) NOT NULL,
 OwnerFirstName nvarchar(max) NOT NULL ,
 OwnerPhone nvarchar(max) NULL,
 OwnerEmail nvarChar(max) NOT NULL,
- Primary Key (OwnerID));
- 
-/*DROP TABLE PET;*/
+Primary Key (OwnerID));
+
 CREATE TABLE PET (
 PetId int not null IDENTITY (1,1) Primary Key,
- PetName nvarchar(max) NOT NULL,
-  PetType nvarchar (max) NOT NULL, 
-  PetBreed nvarchar (max) default 'Unknown',
-   PetDOB datetime2 NULL, 
+PetName nvarchar(max) NOT NULL,
+PetType nvarchar (max) NOT NULL, 
+PetBreed nvarchar (max) default 'Unknown',
+PetDOB datetime2 NULL, 
 PetWeight DECIMAL Not NUll, OwnerID int NOT NULL,
- CONSTRAINT OwnerID_FK Foreign Key (OwnerID) References PET_OWNER(OwnerID) ON DELETE NO ACTION);
+CONSTRAINT OwnerID_FK Foreign Key (OwnerID) References PET_OWNER(OwnerID) ON DELETE NO ACTION);
 
+CREATE TABLE INVOICE 
+(InvoiceNumber int not null IDENTITY (1,1),
+InvoiceDate Datetime2 not null,
+SubTotal float not null,
+TaxPct float not null,
+Total float not null,
+Primary key (InvoiceNumber));
 
-/* DROP TABLE INVOICE;*/
- CREATE TABLE INVOICE 
- (InvoiceNumber int not null IDENTITY (1,1),
-  InvoiceDate Datetime2 not null,
-   SubTotal float not null,
-    TaxPct float not null,
-	 Total float not null,
-	  Primary key (InvoiceNumber));
-
-/*DROP TABLE PRODUCT;  */	 
 Create TABLE PRODUCT (
 ProductNumber Int not null IDENTITY (1,1),
 ProductType nvarchar(max) not null,
 ProductDescription nvarchar (max)not null,
 UnitPrice float not null,
-Primary key ( ProductNumber)
-);
+Primary key ( ProductNumber));
 
-/*DROP TABLE LINE_ITEM;*/
 Create TABLE LINE_ITEM (
 InvoiceNumber int not null,
 LineNumber int not null IDENTITY (1,1),
@@ -47,16 +47,13 @@ Unitprice float not null,
 Total float not null,
 primary key (InvoiceNumber, LineNumber),
 Foreign key (InvoiceNumber) REFERENCES INVOICE(InvoiceNumber),
-FOREIGN KEY (ProductNumber) REFERENCES PRODUCT (ProductNumber)
-);
-
+FOREIGN KEY (ProductNumber) REFERENCES PRODUCT (ProductNumber));
 
 /*SEMINAR(SeminarID, SeminarDate, Location, SeminarTitle)
 	SEMINAR_CUSTOMER(SeminarID, CustomerID)
 	CUSTOMER(CustomerID, FirstName, LastName, Street, Zip)
 	ZIP_CITY(Zip, City)
 	*/
-	
 CREATE TABLE SEMINAR (
 SeminarID int not null,
 SeminarDate datetime2 not null,
@@ -69,6 +66,11 @@ SeminarID int not null,
 CustomerID int not null IDENTITY (1,1),
 PRIMARY KEY ( SeminarID, CustomerID),
 FOREIGN KEY (SeminarID) REFERENCES SEMINAR (SeminarID));
+
+
+
+
+
 
 
 
